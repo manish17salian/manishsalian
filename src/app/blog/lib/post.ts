b1b29af45ee5b003ec2ef7c0328caacb97d94518
manish17salian <manishsalian16@gmail.com>
2025-12-31 11:03:07 +0000

    Changed folder for blogs

--- src/app/blog/lib/post.ts
+++ src/app/blog/lib/post.ts
@@ -4,7 +4,7 @@ import matter from "gray-matter";
 import { remark } from "remark";
 import html from "remark-html";
 
-const postsDir = path.join(process.cwd(), "content/blog");
+const postsDir = path.join(process.cwd(), "blog");
 
 export type PostMeta = {
   slug: string;
