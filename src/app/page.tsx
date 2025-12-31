7d2048d5983c45b727b22517aa8dd21406835828
manish17salian <manishsalian16@gmail.com>
2025-12-31 11:42:00 +0000

    Commented Blog section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function App(){
             <Work/>
             <Qualification/>
             <Contact/>
-            <BlogSection />
+            {/* <BlogSection /> */}
         </main>
     )
 }
